b1b9350b982638cdb9e95e75aa2b9c522ab87992
joonseok kim <mansukim1125@gmail.com>
2025-02-05 20:54:43 +0900

    feat(calculate-subtract): 뺄셈 기능 추가

--- app/src/main/java/com/example/demo/App.java
+++ app/src/main/java/com/example/demo/App.java
@@ -40,6 +40,11 @@ public class App {
                         requestDto,
                         requestDto.getLhs() + requestDto.getRhs()
                     );
+                } else if (requestDto.getOperator().equals("-")) {
+                    responseDto = new CalculationResponseDto(
+                        requestDto,
+                        requestDto.getLhs() - requestDto.getRhs()
+                    );
                 }
 
                 responseStr = mapper.writeValueAsString(responseDto);
